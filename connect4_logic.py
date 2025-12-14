0bb802aea4dfd64999d8cc975ff7f43334dfc82e
Nora-Walid <164080433+Nora-Walid@users.noreply.github.com>
2025-12-14 16:15:37 +0200

    Add files via upload

---
+++ connect4_logic.py
@@ -0,0 +1,66 @@
+import copy
+import random
+
+class Connect4:
+    def __init__(self):
+        pass
+
+    ################################################# CURRENT PLATER ##################################################
+    def current_player(self, state):
+        x = sum(r.count("X") for r in state)
+        o = sum(r.count("O") for r in state)
+        return "X" if x == o else "O"
+
+
+    ################################################# available_actions ###########################################
+    def available_actions(self, state):
+        actions = []
+        player = self.current_player(state)
+        rows, cols = len(state), len(state[0])
+
+        for c in range(cols):
+            if state[0][c] == "":                           # لو اول خانة في العمود دا فاضية
+                for r in range(rows-1, -1, -1):             # هبدأ check من تحت لفوق في الصف 
+                    if state[r][c] == "":
+                        actions.append((player, r, c))
+                        break
+
+        random.shuffle(actions)                               # هلغبط ترتيبهم عشوائيا --> عشان مش كل مره يلعب في نفس العمود , فأخلي اللعبه اكتر واقعية
+        actions.sort(key=lambda a: abs(a[2] - 3))             # هرتب الاكشنو بناءا علي مدي قربها للعمود الاوسط --> لان كل ما كنت بتلعب في النص كل ما كان احسن 
+        return actions
+
+
+    ############################################################ take_action #########################################################
+    def take_action(self, state, action):
+        new_state = copy.deepcopy(state)    # هاخد نسخه من الاستيت تكون منفصله عنها تمام , لما نعدل فيها التعديل ميسمعش في الاصليه ودا باستخدام deepcopy()
+        p, r, c = action
+        new_state[r][c] = p
+        return new_state
+
+
+    ######################################################  check_terminal  ###########################################################
+    def check_terminal(self, state):
+        rows, cols = len(state), len(state[0])
+
+        def win(p):
+            for r in range(rows):                                             # الفوز الافقي 
+                for c in range(cols-3):
+                    if all(state[r][c+i] == p for i in range(4)): return True
+
+            for c in range(cols):                                             # الفوز الرأسي
+                for r in range(rows-3):
+                    if all(state[r+i][c] == p for i in range(4)): return True
+
+            for r in range(rows-3):                                           # الفوز القطري من فوق شمال لتحت يمين 
+                for c in range(cols-3):
+                    if all(state[r+i][c+i] == p for i in range(4)): return True
+
+            for r in range(3, rows):                                          # الفوز القطري من تحت شمال لفوق يمين
+                for c in range(cols-3):
+                    if all(state[r-i][c+i] == p for i in range(4)): return True
+            return False
+
+        if win("X"): return 100000
+        if win("O"): return -100000
+        if all(cell != "" for row in state for cell in row): return 0
+        return "Not terminal"
